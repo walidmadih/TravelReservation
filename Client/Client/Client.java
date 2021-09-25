4745322a45b418dea1b3dca5bc385cf9423315f0
Wawadish <wawadish@gmail.com>
2021-09-25 18:19:41 -0400

    Handling multiple clients

--- Client/Client/Client.java
+++ Client/Client/Client.java
@@ -102,6 +102,7 @@ public abstract class Client
 
 
 				objectOutputStream.writeObject(remoteMethod);
+				objectOutputStream.flush();
 				System.out.println("Flight added");
 			}
 			case AddCars: {
--- Server/Server/Middleware/TCPServer.java
+++ Server/Server/Middleware/TCPServer.java
@@ -8,8 +8,6 @@ import java.net.Socket;
 
 public class TCPServer {
     ServerSocket serverSocket;
-    ObjectOutputStream out;
-    ObjectInputStream in;
 
     public void start(int port){
 
@@ -18,28 +16,60 @@ public class TCPServer {
             System.out.println("Creating TCP Server on port: " + port + ".");
             serverSocket = new ServerSocket(port);
             System.out.println("TCP Server on port: " + port + " has been created. Listening for client connections.");
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Client found. Connected to Client");
-
-            ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
-            ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
-
 
         }catch (IOException e){
             System.out.println("Could not create the server.");
             e.printStackTrace();
         }
+        try {
+            while (true) {
+                new TCPConnection(serverSocket.accept()).run();
+            }
+        }catch (IOException e){
+            System.out.println("Failed to accept client connection.");
+        }
+    }
 
-        while (true) {
+    private class TCPConnection extends Thread {
+        private Socket clientSocket;
+        private ObjectOutputStream out;
+        private ObjectInputStream in;
+
+        TCPConnection(Socket socket){
+            System.out.println("A connection with a client has been initialized.");
+            clientSocket = socket;
+
             try {
-                RemoteMethod remoteMethod = (RemoteMethod) in.readObject();
-                System.out.println(remoteMethod.toString());
-            } catch (IOException e) {
-                e.printStackTrace();
-
-            } catch (ClassNotFoundException e) {
+                out = new ObjectOutputStream(clientSocket.getOutputStream());
+                in = new ObjectInputStream(clientSocket.getInputStream());
+            } catch (IOException e){
+                System.out.printf("Error setting up I/O streams.");
+                System.out.println("The connection with the client has failed.");
                 e.printStackTrace();
             }
+            System.out.println("The connection with the client has been successfully established.");
+        }
+
+        public void run(){
+            try {
+                while (true) {
+                    out.writeObject(executeRemoteMethod(in.readObject()));
+                }
+            }catch (Exception e){
+                System.out.println("Error While executing remote method.");
+                e.printStackTrace();
+            }
+        }
+
+        private Serializable executeRemoteMethod(Object o){
+            if (o instanceof RemoteMethod) {
+                System.out.println("It is a RemoteMethod object");
+                RemoteMethod remoteMethod = (RemoteMethod) o;
+                System.out.println(remoteMethod.toString());
+                return true;
+            }
+            System.out.println("ERROR object is not a RemoteMethod object");
+            return false;
         }
     }
 
