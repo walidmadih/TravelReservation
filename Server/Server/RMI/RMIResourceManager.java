9e53b55955ff088c51fc8d86d1d3d3d27d1cdb36
Allenhuangrui <rui.huang4@mail.mcgill.ca>
2021-09-25 18:26:51 -0400

    fixed port

--- Server/Server/RMI/RMIResourceManager.java
+++ Server/Server/RMI/RMIResourceManager.java
@@ -40,9 +40,9 @@ public class RMIResourceManager extends ResourceManager
 			// Bind the remote object's stub in the registry
 			Registry l_registry;
 			try {
-				l_registry = LocateRegistry.createRegistry(1099);
+				l_registry = LocateRegistry.createRegistry(2034);
 			} catch (RemoteException e) {
-				l_registry = LocateRegistry.getRegistry(1099);
+				l_registry = LocateRegistry.getRegistry(2034);
 			}
 			final Registry registry = l_registry;
 			registry.rebind(s_rmiPrefix + s_serverName, resourceManager);
