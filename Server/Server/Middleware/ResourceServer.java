2545102399a7d131c15ee0072ed2a53f1f6d0d32
Victor Laroche <victor.laroche@mail.mcgill.ca>
2021-09-29 00:51:07 -0400

    Changed resource servers to be nonblocking

--- Server/Server/Middleware/ResourceServer.java
+++ Server/Server/Middleware/ResourceServer.java
@@ -8,48 +8,60 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.net.Socket;
 
-public class ResourceServer extends Thread{
+public class ResourceServer {
 
-    private Socket clientSocket;
     private String address;
-    private ObjectInputStream in;
-    private ObjectOutputStream out;
     private int port;
+
+    private class TempClient {
+
+        private Socket clientSocket;
+        private ObjectInputStream in;
+        private ObjectOutputStream out;
+
+        public TempClient() {
+            connect();
+        }
+
+        private void connect() {
+            try {
+                clientSocket = new Socket(address, port);
+                in = new ObjectInputStream(clientSocket.getInputStream());
+                out =new ObjectOutputStream(clientSocket.getOutputStream());
+            }catch (IOException e){
+                try {
+                    Thread.sleep(500);
+                }catch (InterruptedException ie){
+    
+                }
+                connect();
+            }
+        }
+
+        public Serializable sendRemoteMethod(RemoteMethod remoteMethod){
+            Serializable response = null;
+            try {
+                out.writeObject(remoteMethod);
+                out.flush();
+                response =  (Serializable) in.readObject();
+            }catch (IOException e){
+                e.printStackTrace();
+                System.out.println("Could not communicate remote method to " + address);
+            }catch (ClassNotFoundException e){
+                e.printStackTrace();
+                System.out.println("Could not find readable object " + address);
+            }
+            return response;
+        }
+    }
 
     public ResourceServer(String pAddress, int pPort) {
         address = pAddress;
         port = pPort;
-        connect();
-    }
-
-    private void connect() {
-        try {
-            clientSocket = new Socket(address, port);
-            in = new ObjectInputStream(clientSocket.getInputStream());
-            out =new ObjectOutputStream(clientSocket.getOutputStream());
-        }catch (IOException e){
-            try {
-                Thread.sleep(500);
-            }catch (InterruptedException ie){
-
-            }
-            connect();
-        }
     }
 
     public Serializable sendRemoteMethod(RemoteMethod remoteMethod){
-        Serializable response = null;
-        try {
-            out.writeObject(remoteMethod);
-            out.flush();
-            response =  (Serializable) in.readObject();
-        }catch (IOException e){
-            e.printStackTrace();
-            System.out.println("Could not communicate remote method to " + address);
-        }catch (ClassNotFoundException e){
-            e.printStackTrace();
-            System.out.println("Could not find readable object " + address);
-        }
-        return response;
+        TempClient client = new TempClient();
+        return client.sendRemoteMethod(remoteMethod);
     }
 }
