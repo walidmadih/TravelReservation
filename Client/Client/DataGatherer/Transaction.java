1be6b440bf9db6a63f5ac20f0492f49cd053bd14
Allenhuangrui <rui.huang4@mail.mcgill.ca>
2021-11-08 06:20:34 -0500

    Transaction Manager

--- Client/Client/DataGatherer/Transaction.java
+++ Client/Client/DataGatherer/Transaction.java
@@ -29,7 +29,7 @@ public class Transaction{
         return aSize;
     }
 
-    private void executeAllOperations() throws RemoteException{
+    private void executeAllOperations() throws RemoteException, IResourceManager.InvalidTransactionException, IResourceManager.TransactionAbortedException {
         for(Operation operation : aOperations){
             operation.executeOnClient(aClient);
         }
@@ -44,7 +44,7 @@ public class Transaction{
         //TODO Implement this
     }
 
-    public int start() throws RemoteException{
+    public int start() throws RemoteException, IResourceManager.InvalidTransactionException, IResourceManager.TransactionAbortedException {
         int xid = aClient.startTransaction();
         //try{
         TransactionTimer timer = new TransactionTimer();
